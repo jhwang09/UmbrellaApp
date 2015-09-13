66b8a8f724d801060fdbe6ca40e8654b192c1e57
Jerome Hwang <jhwang09@gmail.com>
2015-09-13 13:33:31 -0400

    Initial Commit

---
+++ Should I Bring An Umbrella?/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Should I Bring An Umbrella?
+//
+//  Created by Jerome Hwang on 9/13/15.
+//  Copyright © 2015 Jerome Hwang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Should I Bring An Umbrella?Tests/Should_I_Bring_An_Umbrella_Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Should_I_Bring_An_Umbrella_Tests.swift
+//  Should I Bring An Umbrella?Tests
+//
+//  Created by Jerome Hwang on 9/13/15.
+//  Copyright © 2015 Jerome Hwang. All rights reserved.
+//
+
+import XCTest
+@testable import Should_I_Bring_An_Umbrella_
+
+class Should_I_Bring_An_Umbrella_Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
